93263799d87f2692b4e2fc41408aa41fe1912968
Steve Binkowski <steve@binkcms.com>
2016-09-08 01:13:58 -0400

    Fix: uninstall script, add JS packages to install, update deployment package

--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,5 @@
 DROP TABLE IF EXISTS `#__nycc_locations`;
+DROP TABLE IF EXISTS `#__nycc_rates`;
+DROP TABLE IF EXISTS `#__nycc_events`;
+DROP TABLE IF EXISTS `#__nycc_venues`;
+DROP TABLE IF EXISTS `#__nycc_venue_rates`;
